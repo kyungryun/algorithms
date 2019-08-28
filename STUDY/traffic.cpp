59c71d818532ac17e9db2466cc3b5a255c5afc25
kyungryun <kyung_ryun@naver.com>
2019-08-28 15:40:24 +0900

    nlogn traffic

--- STUDY/traffic.cpp
+++ STUDY/traffic.cpp
@@ -5,11 +5,17 @@
 
 using namespace std;
 
-pair<long long, int> split(string str){
+struct Log{
+    long long sTime;
+    long long eTime;
+};
+Log split(string str){
+    Log log;
     long long hour = ((str[0] - '0')*10 + (str[1] - '0'))*60*60*1000;
     long long min = ((str[3] - '0')*10 + (str[4] - '0'))*60*1000;
     long long sec = ((str[6] - '0')*10 + (str[7] - '0'))*1000;
     sec += ((str[9]-'0')*100 + (str[10] - '0')*10 + str[11]-'0');
+    long long totalTime = hour+min+sec;
     string t = str.substr(14);
     int time = (str[13]-'0')*1000;
     int ms = 0;
@@ -19,23 +25,25 @@ pair<long long, int> split(string str){
             ms+=(t[i]-'0');
         }
     }
-
-    return make_pair(hour+min+sec, time+ms);
+    ms += time;
+    log.sTime = totalTime - ms + 1;
+    log.eTime = totalTime;
+    return log;
 }
 int solution(vector<string> lines) {
     int answer = 1;
-    vector<pair<long long, int>> v;
+    vector<Log> v;
     for(int i=0 ; i<lines.size() ; i++){
         v.push_back(split(lines[i].substr(11)));
     }
 
-    for(int i=v.front().first-v.front().second+1 ; i<v.back().first ; i+=1000){
-        int s = 0;
-        for(auto it=v.begin() ; it != v.end() ; it++){
-            if(it->first >= i && it->first <= i+1000) s++;
-            else if((it->first - it->second +1) >= i && (it->first - it->second+1) <= i + 1000)s++;
+    for(int i=0 ; i<lines.size()-1 ; i++){
+        int eTime = v[i].eTime;
+        int cnt = 1;
+        for(int j=i+1 ; j<lines.size() ; j++){
+            if(v[j].sTime <= eTime+999 && eTime <= v[j].eTime) cnt++;
         }
-        answer = max(s, answer);
+        answer = max(cnt, answer);
     }
     return answer;
 }
