00fb242e3897077682adb454d81691da40497af4
kyungryun <kyung_ryun@naver.com>
2019-12-13 22:28:07 +0900

    주사위 세개

--- BOJ/2480.cpp
+++ BOJ/2480.cpp
@@ -2,19 +2,16 @@
 
 int main(){
     int arr[3];
-    int MAX = 0;
+    int MAX = 0, ans = 0;;
     for(int i=0 ; i<3 ; i++){
         scanf("%d",&arr[i]);
         if(MAX < arr[i]) MAX = arr[i];
     }
-
-    if(arr[0] == arr[1] && arr[0] == arr[2]){
-        printf("%d\n",10000 + arr[0]*1000);
-    }else if(arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]){
-        printf("%d\n",MAX*100);
-    }else{
-        if(arr[0] == arr[1] || arr[0] == arr[2]) printf("%d\n",1000 + arr[0]*100);
-        else printf("%d\n",1000 +arr[1]*100);
+    if(arr[0] == arr[1] && arr[0] == arr[2]) ans = 10000 + arr[0]*1000;
+    else if(arr[0] != arr[1] && arr[0] != arr[2] && arr[1] != arr[2]) ans = MAX*100;
+    else{
+        if(arr[0] == arr[1] || arr[0] == arr[2]) ans = 1000 + arr[0]*100;
+        else ans = 1000 + arr[1]*100;
     }
-
+    printf("%d\n",ans);
 }
