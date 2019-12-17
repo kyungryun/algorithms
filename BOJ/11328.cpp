a006c9812ad9200392e3001f2ed9381b580589c2
kyungryun <kyung_ryun@naver.com>
2019-12-17 09:32:21 +0900

    Strfry

--- BOJ/11328.cpp
+++ BOJ/11328.cpp
@@ -13,22 +13,17 @@ int main(){
         cin>>t1>>t2;
 
         int a[1001] = {0,};
-        int b[1001] = {0,};
-        if(t1.size() != t2.size()) cout<<"Impossible"<<"\n";
-        else{
-            for(int j=0 ; j<t1.size() ; j++){
-                a[t1[j]-'a']++;
-                b[t2[j]-'a']++;
+        for(int j=0 ; j<t1.size() ; j++) a[t1[j]-'a']++;
+        for(int j=0 ; j<t2.size() ; j++) a[t2[j]-'a']--;
+
+        bool flag = true;
+        for(int j=0 ; j<26 ; j++){
+            if(a[j] != 0){
+                flag = false;
+                break;
             }
-            bool flag = true;
-            for(int j=0 ; j<26 ; j++){
-                if(a[j] != b[j]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) cout<<"Possible"<<"\n";
-            else cout<<"Impossible"<<"\n";
         }
+        if(flag) cout<<"Possible"<<"\n";
+        else cout<<"Impossible"<<"\n";
     }
 }
