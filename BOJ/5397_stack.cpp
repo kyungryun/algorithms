d381c861b4447c5b606cbd234fa299588e80b8dc
kyungryun <kyung_ryun@naver.com>
2019-12-18 10:25:39 +0900

    키로거

--- BOJ/5397_stack.cpp
+++ BOJ/5397_stack.cpp
@@ -1,19 +1,16 @@
-#include <string>
-#include <iostream>
+#include <cstdio>
 #include <stack>
 using namespace std;
 
 stack<char> a;
 stack<char> b;
+char l[1000001];
 int main(){
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
     int t;
-    cin>>t;
-    for(int T=0 ; T<t ; T++){
-        string l;
-        cin>>l;
-        for(int i=0 ; i<l.size() ; i++){
+    scanf("%d",&t);
+    while(t--){
+        scanf("%s",l);
+        for(int i=0 ; l[i] != 0 ; i++){
             char c = l[i];
             if((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9')) a.push(c);
             if(c == '<' && !a.empty()){
@@ -33,9 +30,9 @@ int main(){
             a.pop();
         }
         while(!b.empty()){
-            cout<<b.top();
+            printf("%c",b.top());
             b.pop();
         }
-        cout<<"\n";
+        printf("\n");
     }
 }
