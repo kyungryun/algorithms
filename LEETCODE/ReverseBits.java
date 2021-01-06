8cd47851b337f3b7dee9746f9832a886d9031253
kyungryun <kyung_ryun@naver.com>
2021-01-06 18:58:41 +0900

    190. Reverse Bits

---
+++ LEETCODE/ReverseBits.java
@@ -0,0 +1,15 @@
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for(int i=0 ; i<32 ; i++){
+            ans = ans<<1;
+
+            if((n&1) > 0){
+                ans = ans | 1;
+            }
+            n=n>>>1;
+        }
+        return ans;
+    }
+}
