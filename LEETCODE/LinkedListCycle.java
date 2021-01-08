1c436aaba6cf05db704498ae1ea358b06c20ae96
kyungryun <kyung_ryun@naver.com>
2021-01-08 16:13:27 +0900

    클래스명 수정

--- LEETCODE/LinkedListCycle.java
+++ LEETCODE/LinkedListCycle.java
@@ -1,15 +1,4 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
+public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
--- LEETCODE/PalindromeLinkedList.java
+++ LEETCODE/PalindromeLinkedList.java
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
+class PalindromeLinkedList {
     public boolean isPalindrome(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
