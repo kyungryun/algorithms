8aa404b6b4277fdf35bef494e37648a243756ac8
kyungryun <kyung_ryun@naver.com>
2021-01-14 11:08:19 +0900

    46. Permutations / swap를 이용해서 순열 구하기

--- LEETCODE/Permutations.java
+++ LEETCODE/Permutations.java
@@ -1,26 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 class Permutations {
+//    public List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> ans = new ArrayList<>();
+//        Stack<Integer> curr = new Stack<>();
+//
+//        boolean[] visited = new boolean[nums.length];
+//        perm(nums, ans, curr, visited);
+//        return ans;
+//    }
+//
+//    public void perm(int[] nums, List<List<Integer>> ans, Stack<Integer> curr, boolean[] visited){
+//        if(curr.size() == nums.length){
+//            ans.add(new ArrayList(curr));
+//            return;
+//        }
+//
+//        for(int i=0 ; i<nums.length ; i++){
+//            if(!visited[i]){
+//                visited[i] = true;
+//                curr.push(nums[i]);
+//                perm(nums, ans, curr, visited);
+//                curr.pop();
+//                visited[i] = false;
+//            }
+//        }
+//    }
+
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        perm(nums, ans, curr, visited);
+        perm(nums, 0, ans);
         return ans;
     }
-    
-    public void perm(int[] nums, List<List<Integer>> ans, List<Integer> curr, boolean[] visited){
-        if(curr.size() == nums.length){
-            ans.add(new ArrayList(curr));
-            return;
-        }
-        
-        for(int i=0 ; i<nums.length ; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                curr.add(nums[i]);
-                perm(nums, ans, curr, visited);
-                curr.remove(curr.size()-1);
-                visited[i] = false;
+    public void perm(int[] nums, int idx, List<List<Integer>> ans){
+        if(idx == nums.length){
+            List<Integer> temp = new ArrayList<Integer>();
+            for(int i=0 ; i<nums.length ; i++) temp.add(nums[i]);
+            ans.add(temp);
+        }else{
+            for(int i=idx ; i<nums.length ; i++){
+                swap(nums, idx, i);
+                perm(nums, idx+1, ans);
+                swap(nums, idx, i);
             }
         }
+    }
+    public void swap(int nums[], int a, int b){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
