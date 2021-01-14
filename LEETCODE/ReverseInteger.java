cd7f9462df0d415d3a92557230dcd8683634d746
kyungryun <kyung_ryun@naver.com>
2021-01-14 10:52:04 +0900

    7. Reverse Integer / long타입 안쓴 풀이

--- LEETCODE/ReverseInteger.java
+++ LEETCODE/ReverseInteger.java
@@ -1,14 +1,16 @@
 public class ReverseInteger {
     public int reverse(int x) {
-        long ans = 0;
+        int ans = 0;
 
         while(x != 0){
-            ans *= 10;
-            ans += (x%10);
+            int now = x % 10;
+            int limit = Integer.MAX_VALUE/10;
+            if(ans > Integer.MAX_VALUE/10 || (ans == Integer.MAX_VALUE/10 && now > 7)) return 0;
+            if(ans < Integer.MIN_VALUE/10 || (ans == Integer.MIN_VALUE/10 && now < -8)) return 0;
+            ans = ans*10 + now;
             x/=10;
-        }
-        if(ans < -2147483648 || ans > 2147483647) return 0;
 
-        return (int)ans;
+        }
+        return ans;
     }
 }
