e39a3497fc12e686d722577c5fde5c947342cf03
kyungryun choi <kyung_ryun@naver.com>
2021-02-06 17:36:15 +0900

    불필요한 코드 정리

--- LEETCODE/GasStation.java
+++ LEETCODE/GasStation.java
@@ -5,8 +5,8 @@ class GasStation {
         int idx = 0;
         for(int i=0 ; i<gas.length ; i++){
             int diff = gas[i] - cost[i];
-            tank += gas[i] - cost[i];
-            cur += gas[i] - cost[i];
+            tank += diff;
+            cur += diff;
             if(tank < 0){
                 tank = 0;
                 idx = i+1;
