cbfda99eadae1f8c8139e6788e173ae47d6cd67a
kyungryun choi <kyung_ryun@naver.com>
2021-02-17 15:42:06 +0900

    50. Pow(x, n)

---
+++ LEETCODE/MyPow.java
@@ -0,0 +1,13 @@
+class MyPow {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        double power = myPow(x, n/2);
+        if(n%2 == 0){
+            return power * power;
+        }else{
+            if(n>0) return x*power*power;
+            else return (power*power) / x;
+        }
+      //  return Math.pow(x, n);
+    }
+}
