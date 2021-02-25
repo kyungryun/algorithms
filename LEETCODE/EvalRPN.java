67996cd129693d935b578a72515d4504ac779d00
kyungryun choi <kyung_ryun@naver.com>
2021-02-25 22:29:38 +0900

    150. Evaluate Reverse Polish Notation

---
+++ LEETCODE/EvalRPN.java
@@ -0,0 +1,22 @@
+class EvalRPN {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0 ; i<tokens.length ; i++){
+            if(tokens[i].equals("+")){
+                stack.push(stack.pop() + stack.pop());
+            }else if(tokens[i].equals("-")){
+                stack.push(-stack.pop() + stack.pop());
+            }else if(tokens[i].equals("*")){
+                stack.push(stack.pop() * stack.pop());
+            }else if(tokens[i].equals("/")){
+                int temp = stack.pop();
+                stack.push(stack.pop() / temp);
+            }else{
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        
+        return stack.pop();
+    }
+}
