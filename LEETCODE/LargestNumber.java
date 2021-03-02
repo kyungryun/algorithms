e3df3613fd9eea9712932f33465fbc78afb51c19
kyungryun choi <kyung_ryun@naver.com>
2021-03-02 16:05:08 +0900

    179. Largest Number

---
+++ LEETCODE/LargestNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+        
+        for(int i=0 ; i<nums.length ; i++){
+            arr[i] = Integer.toString(nums[i]);
+        }
+        
+        Arrays.sort(arr, (a, b) ->(b+a).compareTo(a+b));
+        if(arr[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0 ; i<arr.length ; i++){
+            sb.append(arr[i]);
+        }
+        
+        return sb.toString();
+    }
+}
