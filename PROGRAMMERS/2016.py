d01bec8833c8efb77e9eb2cce4e48932791ada67
kyungryun choi <kyung_ryun@naver.com>
2021-04-10 22:45:37 +0900

    프로그래머스/2016

---
+++ PROGRAMMERS/2016.py
@@ -0,0 +1,4 @@
+def solution(a, b):
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return day[(sum(month[:a-1]) + b) % 7 - 1]
