3960ba8018379b5a7b438ab0ffd19b53254a4e1a
kyungryun choi <kyung_ryun@naver.com>
2021-05-06 18:44:49 +0900

    프로그래머스/하샤드수

---
+++ PROGRAMMERS/Harshad.java
@@ -0,0 +1,11 @@
+class Harshad {
+    public boolean solution(int x) {
+        int sum = 0;
+        int tmp = x;
+        while(x > 0){
+            sum += x%10;
+            x /= 10;
+        }
+        return tmp%sum == 0;
+    }
+}
