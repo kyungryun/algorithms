65db839bc181bd8dc3a86625a4a9f4756743188b
kyungryun choi <kyung_ryun@naver.com>
2021-05-25 20:03:37 +0900

    프로그래머스/더맵게

---
+++ PROGRAMMERS/Scoville.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Scoville {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int sc : scoville){
+            pq.offer(sc);
+        }
+        while(pq.peek() <= K){
+            if(pq.size() == 1 ) return -1;
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.offer(a + b*2);
+            answer++;
+        }
+        return answer;
+    }
+}
