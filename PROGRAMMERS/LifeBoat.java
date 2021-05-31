c23fb62aac0953590e1f585434324d75403f91f6
kyungryun choi <kyung_ryun@naver.com>
2021-05-31 20:03:03 +0900

    프로그래머스/구명보트

---
+++ PROGRAMMERS/LifeBoat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class LifeBoat {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int min = 0;
+        for(int i=people.length - 1 ; i >= min ; i--){
+            if(people[min] + people[i] <= limit) min++;
+            answer++;
+        }
+        return answer;
+    }
+}
