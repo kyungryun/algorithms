755eb0b7484460dbd44306bb1249930ab384bd1b
kyungryun choi <kyung_ryun@naver.com>
2021-06-23 17:02:40 +0900

    58. Length of Last Word

---
+++ LEETCODE/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s == null || s.length() == 0) return 0;
+        int ans = 0;
+        boolean flag = false;
+        for(int i=s.length()-1 ; i>=0 ; i--){
+            char c = s.charAt(i);
+            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+                flag = true;
+                ans++;
+            }else if(flag) return ans;
+        }
+        
+        return ans;
+    }
+}
